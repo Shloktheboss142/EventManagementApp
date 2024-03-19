916148828d8b6b5d42c1266b36aea17c30c97090
Shlok Arjun Marathe <shlokmarathe@outlook.com>
2024-03-20 00:15:35 +0800

    New Category UI done

--- app/src/main/java/com/fit2081/assignment1/NewEventCategory.java
+++ app/src/main/java/com/fit2081/assignment1/NewEventCategory.java
@@ -1,8 +1,6 @@
 package com.fit2081.assignment1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Dashboard extends AppCompatActivity {
+public class NewEventCategory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_new_event_category);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onClickNewEventCategory(View view) {
-        Intent intent = new Intent(this, NewEventCategory.class);
-
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/fit2081/assignment1/UserLogin.java
+++ app/src/main/java/com/fit2081/assignment1/UserLogin.java
@@ -5,8 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -31,7 +29,7 @@ public class UserLogin extends AppCompatActivity {
             return insets;
         });
 
-        etUsernameInput = findViewById(R.id.usernameInput);
+        etUsernameInput = findViewById(R.id.eventId);
         etPasswordInput = findViewById(R.id.passwordInput);
 
         SharedPreferences sharedPreferences = getSharedPreferences(KeyStore.FILE_NAME, MODE_PRIVATE);
